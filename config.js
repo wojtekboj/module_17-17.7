eb434103421bef98d526bdd80fa955a6bb4562a9
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-30 10:02:07 +0200

    Add project files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '792597676806-1phq9iinbbimhipjtamlf9rg6u332iks.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'khLpppMTSDLp1WCOdmZBKQz1',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
